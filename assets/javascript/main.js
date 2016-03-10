40117aabefd70f5c45db6ce8e0c70c50ad89acbc
Nick Titcombe <me@nicktitcombe.com>
2016-03-10 10:48:00 -0800

    finished longXor

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -13,10 +13,44 @@ document.addEventListener('DOMContentLoaded', function() {
       return output;
   }
 
+  function padWithZeros(numberToPad, endLength) {
+      while (numberToPad.length < endLength) {
+          numberToPad += "0";
+      }
+      return numberToPad;
+  }
+
+  // Split a string into chunks of four
+  function splitToPieces(string) {
+      var container = [];
+      while (string.length > 0) {
+          container.push(string.slice(0, 4));
+          string = string.slice(4, string.length);
+      }
+      return container;
+  }
+
+  function largeXor(numberOne, numberTwo) {
+      var fullXor = "";
+      if (numberOne.length < numberTwo.length) {
+          numberOne = padWithZeros(numberOne, numberTwo.length);
+      } else {
+          numberTwo = padWithZeros(numberTwo, numberOne.length);
+      }
+      var arrOne = splitToPieces(numberOne);
+      var arrTwo = splitToPieces(numberTwo);
+
+      for (var i = 0; i < arrOne.length; i++) {
+          fullXor += String(Number(arrOne[i]) ^ Number(arrTwo[i]));
+      }
+
+      return fullXor;
+  }
+
   digestButton.click( function() {
       var binarySalt = convertToBinary(salt.val());
       var binaryInput = convertToBinary(input.val());
-      var xor = String(binarySalt ^ binaryInput);
+      var xor = largeXor(binarySalt, binaryInput);
       digestBox.val(window.Sha256.hash(xor));
   });
 });
